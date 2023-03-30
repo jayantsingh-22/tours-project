8320a71baf2f50b71d15d35f74f37be45d4a85c0
jayantsingh-22 <jayantsingh640@gmail.com>
2023-03-30 17:55:31 +0530

    changedPasswordAfter method added

--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,10 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: {
+    type: Date,
+    select: true,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -52,6 +56,19 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  //False means password NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
