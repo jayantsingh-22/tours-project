576681209bb1eca6f488d90481747315aee0d002
jayantsingh-22 <jayantsingh640@gmail.com>
2023-05-26 00:45:50 +0530

    fixed:return middleware issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -117,48 +117,28 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 // Only for rendered pages, no errors!
-
 exports.isLoggedIn = catchAsync(async (req, res, next) => {
-  //1) Getting token and check if it's there
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
-  }
+  //1) Verify Token
 
-  if (!token) {
-    return next(
-      new AppError('You are not logged in! Please log in to get access.', 401)
-    );
-  }
-
-  //2) Verification token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
-
-  //3) Check if user still exists
-  const currentUser = await User.findById(decoded.id);
-  if (!currentUser)
-    return next(
-      new AppError(
-        'The user belonging to this token does no longer exist.',
-        401
-      )
+  if (req.cookies.jwt) {
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
     );
 
-  //4) Check if user changed password after the token was issued
-  if (currentUser.changedPasswordAfter(decoded.iat)) {
-    return next(
-      new AppError('User recently changed password! Please log in again.', 401)
-    );
-  }
+    //2) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) return next();
 
-  //GRANT ACCESS TO PROTECTED ROUTE
-  req.user = currentUser;
+    //3) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    //THERE IS A LOGGED IN USER
+    res.locals.user = currentUser;
+    return next();
+  }
   next();
 });
 
