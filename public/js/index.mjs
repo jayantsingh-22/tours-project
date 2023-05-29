399df57aa685a6393b84f9054d4f3eec65d3069b
jayantsingh-22 <jayantsingh640@gmail.com>
2023-05-29 11:47:52 +0530

    added:updateSettings method

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -4,11 +4,14 @@ import 'regenerator-runtime/runtime';
 
 import { displayMap } from './mapbox.mjs';
 import { login, logout } from './login.mjs';
+import { updateSettings } from './updateSettings.mjs';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 //VALUES
 
@@ -28,3 +31,29 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = ''; 
+  });
+}
