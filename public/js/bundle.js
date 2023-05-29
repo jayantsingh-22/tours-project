9ed50aa744195b8e5db16aa1563df15947939b20
jayantsingh-22 <jayantsingh640@gmail.com>
2023-05-29 12:33:06 +0530

    package bundler updated

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -22489,7 +22489,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51724" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52364" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
