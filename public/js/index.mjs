3c6276c84aabe7f1f32f8099489c67a615dc6e83
jayantsingh-22 <jayantsingh640@gmail.com>
2023-06-05 16:23:26 +0530

    added:signup functionality

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -3,13 +3,14 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import { displayMap } from './mapbox.mjs';
-import { login, logout } from './login.mjs';
+import { login, logout, signup } from './login.mjs';
 import { updateSettings } from './updateSettings.mjs';
 import { bookTour } from './stripe.mjs';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
+const signUpForm = document.querySelector('.form--signup');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
@@ -29,6 +30,18 @@ if (loginForm) {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
+  });
+}
+
+if (signUpForm) {
+  signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+
+    signup(name, email, password, passwordConfirm);
   });
 }
 
@@ -61,12 +74,12 @@ if (userPasswordForm) {
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
   });
-} 
+}
 
-if(bookBtn) {
-  bookBtn.addEventListener('click', e => {
+if (bookBtn) {
+  bookBtn.addEventListener('click', (e) => {
     e.target.textContent = 'Processing...';
-    const {tourId} = e.target.dataset;
+    const { tourId } = e.target.dataset;
     bookTour(tourId);
-  })
+  });
 }
