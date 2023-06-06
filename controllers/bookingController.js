0eaee90a212a7cc747613c0d104ae2b875cba672
jayantsingh-22 <jayantsingh640@gmail.com>
2023-06-06 16:33:28 +0530

    improved:stripe implementation 3rd time

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -37,7 +37,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           },
         },
         quantity: 1,
-        expand: ['price_data'],
       },
     ],
     mode: 'payment',
@@ -63,7 +62,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.data[0].object.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
